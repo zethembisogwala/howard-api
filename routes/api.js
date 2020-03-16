cadde975f32e14d59334354b296ab2cc767bc0b2
Zethembiso Gwala <zethembisogwala@yahoo.com>
2020-03-16 02:19:24 +0200

    login + token init

--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const jwt = require('jsonwebtoken')
 const router = express.Router()
 const User = require('../models/user')
 const mongoose = require('mongoose')
@@ -25,7 +26,34 @@ router.post('/register', (req, res) => {
 			console.log(err)
 		}
 		else{
-			res.status(200).send(registeredUser)
+			let payload = { subject: registeredUser._id }
+			let token = jwt.sign(payload, 'secretKey')
+			res.status(200).send({token})
+		}
+	})
+})
+
+router.post('/login', (req, res) => {
+	let userData = req.body
+
+	User.findOne({email: userData.email}, (err, user) => {
+		if(err){
+			console.log(err)
+		}
+		else{
+			if(!user){
+				res.status(401).send('Invalid email')
+			}
+			else{
+				if(user.password !== userData.password){
+					res.status(401).send('Invalid password')
+				}
+				else{
+					let payload = { subject: user._id }
+					let token = jwt.sign(payload, 'secretKey')
+					res.status(200).send({token})
+				}
+			}
 		}
 	})
 })
