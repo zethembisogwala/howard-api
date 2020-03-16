6ebaeed8847b95c69fb380e652431d034a13aff3
Zethembiso Gwala <zethembisogwala@yahoo.com>
2020-03-16 18:51:01 +0200

    auth guard

--- routes/api.js
+++ routes/api.js
@@ -14,6 +14,23 @@ mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true  }, err =
 	}
 })
 
+function verifyToken(req, res, next){
+	if(!req.headers.authorization){
+		return res.status(401).send('Unauthorized request')
+	}
+	let token = req.headers.authorization.split(' ')[1]
+	if(token === 'null'){
+		return res.status(401).send('Unauthorized request')
+	}
+	let payload = jwt.verify(token, 'secretKey')
+	if(!payload){
+		return res.status(401).send('Unauthorized request')
+	}
+	req.userId = payload.subject
+	next()
+} //implement this function as second argument to requests that require token validation
+// router.get('/special', verifyToken, (req, res) => ...)
+
 router.get('/', (req, res) => {
 	res.send('From API route')
 })
